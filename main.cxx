0fb5eafdfc36ea85cc1a2d9ea66a6894b4d94f52
Foxyman-sama <dimalatoho1448@gmail.com>
2024-02-27 19:37:16 +0200

    First release

--- main.cxx
+++ main.cxx
@@ -32,6 +32,7 @@ int main(int argc, char **argv) {
 
 #else
 
+#include <filesystem>
 #include <format>
 #include <iostream>
 
@@ -41,27 +42,34 @@ int main(int argc, char **argv) {
 #include "modules/webserver/html_parser.hpp"
 #include "modules/webserver/webserver.hpp"
 
+void createDirectories() {
+  std::filesystem::create_directory("storage");
+  std::filesystem::create_directory("htmls");
+}
+
 int main() {
+  createDirectories();
+
   net::io_context io;
-  home::container::JSONContainer container { "build/data.json" };
+  home::container::JSONContainer container { "storage/data.json" };
   home::interactor::InteractorImpl interactor { container };
   home::webserver::HTMLParser parser;
   home::controller::ControllerImpl controller { interactor, parser };
   home::webserver::HTMLContainer htmls;
-  htmls.emplace("/", "build/index.html");
+  htmls.emplace("/", "htmls/index.html");
 
   home::webserver::WebServer server { io, 9090, controller, htmls };
   while (true) {
     auto socket { server.accept() };
+    socket.set_option(net::socket_base::reuse_address(true));
     while (true) {
       try {
         server.handle(socket);
       } catch (...) {
-        std::cerr << std::format("{} is dropped\n", socket.remote_endpoint().address().to_string());
         break;
       }
     }
-
+    // I LOVE YOU MUCH MORE! DON'T ARGUE ABOUT IT!
     net::error_code ec;
     socket.shutdown(net::tcp::socket::shutdown_both, ec);
     container.extractToFile();
